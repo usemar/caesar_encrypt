c5142d20cb17b4501e26054de25a35775eec2cf9
usemar <99183981+usemar@users.noreply.github.com>
2024-06-15 20:32:59 +0200

    Update caesar_encrypt.py
    Created on Thu Jun 13 09:31:42 2024
    This Code creates an encrypted Text from your Input.
    It works only with the Chars in the list "alpha_b". 
    You havte to give a Keynumber to shift the Alphabet.
    For Example a = index 0 , key = 2 , the Alphabet will start at c and a,b will be added to the end

--- caesar_encrypt.py
+++ caesar_encrypt.py
@@ -5,7 +5,8 @@ Filename: caesar_encrypt.py
 Created on Thu Jun 13 09:31:42 2024
 This Code creates an encrypted Text from your Input.
 It works only with the Chars in the list "alpha_b". 
-You havte to give a Keynumber for the Ammount for shifting the Alphabet.
+You havte to give a Keynumber to shift the Alphabet.
+For Example a = index 0 , key = 2 , the Alphabet will start at c and a,b will be added to the end
 @author: Ulrich Semar
 """
 alpha_b = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," "]
